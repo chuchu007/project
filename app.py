34b50677fb971d9d1525616d90d21b3cadee4ac1
Benneth Wisdom <57001542+chuchu007@users.noreply.github.com>
2025-07-11 12:58:31 -0400

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/heimdall.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/heimdall.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 from models_db import db, User
